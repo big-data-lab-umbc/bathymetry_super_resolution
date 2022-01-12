de4dd2ca689c920580f8f6d98de9e85249cf3eb2
lixy4567 <90643297+lixy4567@users.noreply.github.com>
2022-01-12 10:27:31 -0500

    update on dataloader

--- srgan/datasets.py
+++ srgan/datasets.py
@@ -2,6 +2,7 @@ import glob
 import random
 import os
 import numpy as np
+import rasterio
 
 import torch
 from torch.utils.data import Dataset
@@ -15,6 +16,7 @@ mean = np.array([0.5,])
 std = np.array([0.5,])
 
 
+#Dataloader for training 
 class ImageDataset(Dataset):
     def __init__(self, root, hr_shape):
         hr_height, hr_width = hr_shape
@@ -23,29 +25,73 @@ class ImageDataset(Dataset):
             [
                 #transforms.Resize((hr_height // 4, hr_height // 4), Image.BICUBIC),
                 transforms.ToTensor(),
-                transforms.Normalize(mean, std),
+###                transforms.Normalize(mean, std),
             ]
         )
         self.hr_transform = transforms.Compose(
             [
                 #transforms.Resize((hr_height, hr_height), Image.BICUBIC),
                 transforms.ToTensor(),
-                transforms.Normalize(mean, std),
+###                transforms.Normalize(mean, std),
             ]
         )
 
-        self.files_hr = sorted(glob.glob(root + "img_hr/*.*"))
-        self.files_lr = sorted(glob.glob(root + "img_lr/*.*"))
+        self.files_hr = sorted(glob.glob(root + "img_hr_tif/*.*"))
+        self.files_lr = sorted(glob.glob(root + "img_lr_tif/*.*"))    #for training
 
     def __getitem__(self, index):
-        img_hr = Image.open(self.files_hr[index % len(self.files_hr)]).convert('L')
-        img_lr = Image.open(self.files_lr[index % len(self.files_lr)]).convert('L')
+###        img_hr = Image.open(self.files_hr[index % len(self.files_hr)]).convert('L')
+###        img_lr = Image.open(self.files_lr[index % len(self.files_lr)]).convert('L')
+        img_hr = rasterio.open(self.files_hr[index % len(self.files_hr)])
+        img_lr = rasterio.open(self.files_lr[index % len(self.files_lr)])
         #print(self.files_lr[index % len(self.files_lr)])
         #print(self.files_hr[index % len(self.files_hr)])
+
+        img_hr = img_hr.read(1)
+        img_hr=np.float32(img_hr)
+        img_lr = img_lr.read(1)
+        img_lr=np.float32(img_lr)
+
+        
         image_lr = self.lr_transform(img_lr)
         image_hr = self.hr_transform(img_hr)
+
+        image_hr = (image_hr+2297)/8659
+        image_lr = (image_lr+2297)/8659
 
         return {"lr": image_lr, "hr": image_hr}
 
     def __len__(self):
         return len(self.files_hr)
+
+
+#Dataloader for testing
+class ImageDataset_test(Dataset):
+    def __init__(self, root, img_shape):
+        img_height, img_width = img_shape
+        # Transforms for low resolution images and high resolution images
+        self.lr_transform = transforms.Compose(
+            [
+                #transforms.Resize((hr_height // 4, hr_height // 4), Image.BICUBIC),
+                transforms.ToTensor(),
+                transforms.Normalize(mean, std),
+            ]
+        )
+        
+        self.files_lr = sorted(glob.glob(root + "/*.*"))    
+
+    def __getitem__(self, index):
+
+        img_lr = rasterio.open(self.files_lr[index % len(self.files_lr)])
+
+        img_lr = img_lr.read(1)
+        img_lr=np.float32(img_lr)
+
+        image_lr = self.lr_transform(img_lr)
+
+        image_lr = (image_lr+2297)/8659
+
+        return {"lr": image_lr}
+
+    def __len__(self):
+        return len(self.files_lr)
